73e845f47431b0b737c8ef619597f5a8b8d26a18
Jan Horak <janny_b_goode@hotmail.com>
2022-07-02 23:54:11 -0400

    add more styling

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import '../../styles/landing.css'
+import "../../styles/landing.css";
 
 const Landing = () => {
   return (
@@ -9,7 +9,11 @@ const Landing = () => {
         <h1>Letter Boxed Solver</h1>
       </div>
       <div className="solver-container">
-        <h2>Letter box goes here</h2>
+        <h3>letter input goes here</h3>
+        <div className="white-box">
+          <h2>Letter box goes here</h2>
+        </div>
+        <h3>letter input goes here</h3>
       </div>
       <div className="solution-suggestions">
         <h3> Suggestions</h3>

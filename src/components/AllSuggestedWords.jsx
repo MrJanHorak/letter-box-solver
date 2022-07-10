f4c37b8810d84e70c9061443cca8cd1b7d22eb84
Jan Horak <janny_b_goode@hotmail.com>
2022-07-10 15:18:28 -0400

    fix display of suggested words

--- src/components/AllSuggestedWords.jsx
+++ src/components/AllSuggestedWords.jsx
@@ -4,12 +4,18 @@ import "../styles/allSuggestedWords.css";
 
 const AllSuggestedWords = ({ potentialWords }) => {
   let suggestionsArray = [];
-  Object.keys(potentialWords).map((word, index) => suggestionsArray.push(word));
+  Object.keys(potentialWords).map((word) => suggestionsArray.push(word));
+
+  const listItems = suggestionsArray.map((word, index) => {
+    return(<li key={index}>{word}</li>)
+  })
 
   return (
     <>
       <h3>Possible solutions:</h3>
-      <div className="all-suggested-words">{suggestionsArray}</div>
+      <div className="all-suggested-words">
+        <ul>{listItems}</ul>
+      </div>
     </>
   );
 };
